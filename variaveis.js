6ac58f455420a7580c56b78bd2ca28da2b6fd4a7
George Mayko <mayko.george@gmail.com>
2016-11-10 15:28:23 -0300

    Create variaveis.js

---
+++ variaveis.js
@@ -0,0 +1,13 @@
+//Variáveis de Escopo de instância
+var nome = "Quadrado";
+var quantidadeDeLados = 4;
+var tamanhoDoLado = 3;
+
+//Variáveis de bloco
+let nome = "Triangulo Equilatero"
+let quantidadeDeLados = 3;
+let tamanhoDoLado = 4;
+
+//Variáveis constantes
+const NOTA_MAXIMA = 10;
+const NOTA_MINIMA = 0;
